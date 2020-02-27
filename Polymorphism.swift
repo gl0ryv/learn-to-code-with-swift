edcf8ea0dd53a3eb906d398e248abdff86d4baf5
Slava Volodin <volodinvs_iOSDev@icloud.com>
2020-02-27 20:50:21 +0700

    Add files via upload

---
+++ Polymorphism.swift
@@ -0,0 +1,26 @@
+//: Swift 5.1
+
+import UIKit
+
+class Shape {
+    var area: Double?
+    
+    func calculateArea(valA: Double, valB: Double) {
+        
+    }
+}
+
+class Triangle: Shape {
+    
+    override func calculateArea(valA: Double, valB: Double) {
+        area = (valA * valB) / 2
+    }
+}
+
+class Rectangle: Shape {
+    
+    override func calculateArea(valA: Double, valB: Double) {
+        
+        area = (valA * valB)
+    }
+}
